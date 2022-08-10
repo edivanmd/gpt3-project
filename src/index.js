aac1fabf586c1711ec62b14eddc532c460dd3032
edivanmd <edivanmd@gmail.com>
2022-08-10 15:45:53 +1000

    Structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App'
+import App from './App';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
