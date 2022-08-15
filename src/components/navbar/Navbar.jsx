9f2916a059912f3a3f1e0dbdd02d51e2b126d666
edivanmd <edivanmd@gmail.com>
2022-08-15 17:24:16 +1000

    Section: What is GPT3

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import './navbar.css';
 const Menu = () => (
   <>
     <p><a href="#home">Home</a></p>
-    <p><a href="#wght3">What is GPT?</a></p>
+    <p><a href="#whatgpt3">What is GPT?</a></p>
     <p><a href="#possibility">Open AI</a></p>
     <p><a href="#features">Case Studies</a></p>
     <p><a href="#blog">Library</a></p>
